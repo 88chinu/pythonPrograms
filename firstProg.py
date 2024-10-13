a3be254d46cf1747f7316ef5966bc9a6edade6db
88chinu <chinmaya.pa42@gmail.com>
2024-10-13 21:15:54 +0530

    first commit

---
+++ firstProg.py
@@ -0,0 +1,46 @@
+#Learing Python
+print ('Hello', 'world')
+
+#Print using variable
+w = 10
+x = 5
+Y = "John"
+z = 8.765
+print(x+w)
+print(type(x+w))
+print(type(x))
+print(type(Y))
+print(type(z))
+
+#use of if statement
+if (x<z):
+  print("z is greater than x") 
+
+
+#Object statement
+friutes = ["apple", "banana", "orange"] 
+f = ("apple", "banana", "orange")
+a, b, c = friutes
+print(a)
+print(b)
+print(c)
+print(type(friutes))
+print(type(f))
+
+#Global variable
+q = "awesome"  
+def myfunc():
+    global p
+    p = "fantastic"
+myfunc()
+
+print("Python is " + p)
+print("Python is " + q + " and " + p)
+
+#Complex datatype
+real = 3
+imaginary = 5
+
+
+z = complex(real, imaginary)
+print("The complex number is ", z)
