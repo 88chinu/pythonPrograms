11c61f6ef436aa0d803a4bba1a6d9a67b4b4cfa5
88chinu <chinmaya.pa42@gmail.com>
2024-10-14 19:57:07 +0530

    some program tasks

---
+++ practicePrograms.py
@@ -0,0 +1,41 @@
+# Find the sum of two number
+a = int(input("Enter 1st num: "))
+b = int(input("Enter 2nd num: "))
+
+print("sum of two num is", a+b)
+
+
+# Find the reminder of the given numbers
+
+c = int(input("Enter the Dividend: "))
+d = int(input("Enter the Divisor: "))
+
+print("The Remainder is: ", c%d )
+
+
+# Find the greatest value 
+
+p = int(input("Enter 1st num: "))
+q = int(input("Enter 2nd num: "))
+
+if (p>q):
+    print("P is greatest num in given")
+elif (p<q):
+    print("Q is greatest num in given")
+else :
+    print("The given numbers are Equal")
+
+
+# Find the average of two num
+
+x = int(input("Enter 1st num: "))
+y = int(input("Enter 2nd num: "))
+
+print("The average of the two num is: ", (x+y)/2 )
+
+
+# Find the Square of the number
+
+m = int(input("Enter the number: "))
+
+print("The square of the number is: ", m**2 )
